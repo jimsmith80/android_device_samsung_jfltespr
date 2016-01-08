7112829ca80e9514754191c482274f333a6ee8b3
jimsmith80 <jimsmith80@users.noreply.github.com>
2016-01-08 08:38:09 -0700

    update fingerprint and build description

--- carbon.mk
+++ carbon.mk
@@ -6,8 +6,8 @@ $(call inherit-product, vendor/carbon/config/common_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jfltespr \
     TARGET_DEVICE=jfltespr \
-    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.4.2/KOT49H/L720VPUFNG2:user/release-keys" \
-    PRIVATE_BUILD_DESC="jfltespr-user 4.4.2 KOT49H L720VPUFNG2 release-keys"
+    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPUGOH1:user/release-keys" \
+    PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPUGOH1 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
